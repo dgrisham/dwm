2e7e364b6a8d948231884c71e8f24cdded6e5908
David Grisham <dmgrisham@gmail.com>
2019-07-02 10:26:06 -0600

    chrome shortcut

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static const char* Logfile = NULL;
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[]  = { "google-chrome-stable", NULL };
+static const char *browsercmd[]  = { "chrome", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function           argument */
