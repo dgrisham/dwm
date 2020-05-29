58532a33aff0847d79dd0743c144aea8c857469b
dgrisham <dmgrisham@gmail.com>
2020-05-29 17:04:01 -0600

    Toggle fullscreen; remap a few keybindings

--- config.h
+++ config.h
@@ -63,15 +63,17 @@ static const char* Logfile = NULL;
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[]  = { "chrome", NULL };
+static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]     = { "urxvt", NULL };
+static const char *browsercmd[]  = { "chromium", NULL };
+static const char *musiccmd[]    = { "spotify", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function           argument */
-	{ MODKEY,                       XK_p,      spawn,             {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,             {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,             {.v = termcmd } },
 	{ MODKEY,                       XK_b,      spawn,             {.v = browsercmd } },
+	{ MODKEY,                       XK_m,      spawn,             {.v = musiccmd } },
 	{ MODKEY,                       XK_s,      togglebar,         {0} },
 	{ MODKEY,                       XK_j,      focusstack,        {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,        {.i = -1 } },
@@ -79,12 +81,13 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,        {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,          {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,          {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,     {0} },
 	{ MODKEY,                       XK_Return, zoom,              {0} },
 	{ MODKEY,                       XK_Tab,    view,              {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,        {0} },
-	{ MODKEY,                       XK_t,      setlayout,         {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,         {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,         {.v = &layouts[2]} },
+	{ MODKEY,                       XK_F1,     setlayout,         {.v = &layouts[0]} },
+	{ MODKEY,                       XK_F2,     setlayout,         {.v = &layouts[1]} },
+	{ MODKEY,                       XK_F3,     setlayout,         {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,             XK_m,      monocle_togglebar, {0} },
 	{ MODKEY,                       XK_space,  setlayout,         {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating,    {0} },
--- dwm.c
+++ dwm.c
@@ -212,6 +212,7 @@ static void tagmon(const Arg *arg);
 static void tile(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
 static void unfocus(Client *c, int setfocus);
@@ -733,9 +734,11 @@ drawbar(Monitor *m)
 
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
-    		int mid = (m->ww - TEXTW(m->sel->name) - x) / 2;
-			drw_setscheme(drw, scheme[m == selmon ? SchemeSelWin : SchemeNorm]);
-			drw_text(drw, x, 0, w, bh, mid, m->sel->name, 0);
+            drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
+            drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
+			// int mid = (m->ww - TEXTW(m->sel->name) - x) / 2;
+			// drw_setscheme(drw, scheme[m == selmon ? SchemeSelWin : SchemeNorm]);
+			// drw_text(drw, x, 0, w, bh, mid, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
@@ -1465,6 +1468,19 @@ setfullscreen(Client *c, int fullscreen)
 	}
 }
 
+Layout *last_layout;
+void
+fullscreen(const Arg *arg)
+{
+	if (selmon->showbar) {
+		for(last_layout = (Layout *)layouts; last_layout != selmon->lt[selmon->sellt]; last_layout++);
+		setlayout(&((Arg) { .v = &layouts[2] }));
+	} else {
+		setlayout(&((Arg) { .v = last_layout }));
+	}
+	togglebar(arg);
+}
+
 void
 setlayout(const Arg *arg)
 {
@@ -1695,6 +1711,13 @@ togglefloating(const Arg *arg)
 		resize(selmon->sel, selmon->sel->x, selmon->sel->y,
 			selmon->sel->w, selmon->sel->h, 0);
 	arrange(selmon);
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+    if (selmon->sel)
+        setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
